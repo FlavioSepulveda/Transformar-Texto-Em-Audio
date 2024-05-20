ac46b8e459717c98c05ed8457227b38ef272ddc4
FlavioSepulveda <115578503+FlavioSepulveda@users.noreply.github.com>
2024-05-19 21:45:41 -0300

    Salvando o Arquivo

---
+++ main.py
@@ -0,0 +1,21 @@
+# Transformando Texto em Audio
+from PyPDF2 import PdfReader
+from gtts import gTTS
+
+# Função que converte PDF em Audio:
+def pdf_to_audio(pdf):
+    # Abrindo o arquivo -
+    reader = PdfReader(pdf)
+    text_total = ''
+    
+    # Extraindo os textos de cada pagina -
+    for pages in reader.pages:
+        text_total += pages.extract_text() + ' '
+        
+    # Convertendo texto para audio
+    tts = gTTS(text=text_total, lang='pt')
+    tts.save('Audio.mp3')
+    
+if __name__=='__main__':
+    path_pdf = 'Coloque aqui o caminho do pdf.'
+    pdf_to_audio(path_pdf)
